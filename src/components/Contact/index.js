933bdabc0e0d96bed7bf42bd46ededd87a3fb9db
skeny.patel <skeny.patel@servicenow.com>
2023-10-24 22:33:36 +0530

    emailjs setup

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,13 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 const Contact = () => {
  const [letterClass, setLetterClass] = useState('text-animate');
+ const refForm = useRef();
  useEffect(() => {
   const asyncFunc = async () => {return setTimeout(() => {
       setLetterClass('text-animate-hover')
@@ -11,6 +15,19 @@ const Contact = () => {
   asyncFunc();
 }, []);
 
+const sendEmail = (e) => {
+  e.preventDefault();
+  emailjs.sendForm('service_rp08yni', 'template_2l3dbob', e.target, '03ORHRYvG-aEMGuMb')
+  .then(
+    () => {
+      alert('Message successfully sent!');
+      window.location.reload(false);
+    },
+    () => {
+      alert('Failed to send the message, please try again.');
+    }
+  );
+}
  return (
   <>
   <div className='container contact-page'>
@@ -22,7 +39,7 @@ const Contact = () => {
      I don't feel like writing this just yet, some other time maybe.
     </p>
     <div className='contact-form'>
-     <form>
+     <form redf={refForm} onSubmit={sendEmail}>
       <ul>
        <li className='half'>
         <input type='text' name='name' placeholder='Name' required />
@@ -43,7 +60,22 @@ const Contact = () => {
      </form>
     </div>
    </div>
+   <div className='info-map'>
+    Skeny Patel
+    <br/>
+    India
+    <br/>
+    <span>skeny27@gmail.com</span>
+   </div>
+   <div className='map-wrap'>
+    <MapContainer center={[21.7051, 72.9959]} zoom={13}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      <Marker position={[21.7051, 72.9959]}>
+        <Popup>I want to break free...</Popup>
+      </Marker>
+    </MapContainer>
 
+   </div>
   </div>
   <Loader type="pacman" />
   </>
